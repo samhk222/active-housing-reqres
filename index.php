fb44dc17da31a21a73685525bb0cffc34b706b00
Samuel Ideapad 3 <samuca@samuca.com>
2022-06-28 23:56:15 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include("vendor/autoload.php");
+
+use \Samhk222\ActiveHousingReqres\Models\V1\User;
+
+$cool_user = (new User)->getById(3);
+$users = (new User)->getUsers(2);
+
+dd($cool_user, $users);
+
